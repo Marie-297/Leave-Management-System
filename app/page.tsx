ded089204003ae7e01dd783c8182e0433b6e28e8
Michael Adonu <adonumichael@gmail.com>
2024-08-20 14:55:05 -0700

    hello

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-
-
 export default function Home() {
-  return (
-    <main className="">
-     <div>Home</div>
-    </main>
-  )
+	return (
+		<main className=''>
+			<div>Home</div>
+			<h1>Hello Mariama</h1>
+		</main>
+	);
 }
